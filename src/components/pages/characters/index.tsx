f953c0d43641c1bdc142e130998af49801bc0cdb
Alejo Gonzalez <alejo.gonzalez@radiumrocket.com>
2023-08-31 17:23:46 -0300

    Add pagination in character list and sort method

--- src/components/pages/characters/index.tsx
+++ src/components/pages/characters/index.tsx
@@ -2,27 +2,72 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import styles from "./characters.module.css";
 import Card from "../../shared/card";
+import Button from "../../shared/button";
+import { RootState, useAppSelector } from "../../../redux/store";
 
 const Characters = () => {
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [sort, setSort] = useState("name");
+
+  const { authUser: token } = useAppSelector((state: RootState) => state.auth);
 
   useEffect(() => {
-    const url = `${process.env.REACT_APP_API_URL}/characters/name`;
+    const url = `${process.env.REACT_APP_API_URL}/characters/${sort}/${page}`;
+
+    const headers = {
+      token: token.token,
+    };
+
     axios
-      .get(url)
+      .get(url, { headers })
       .then((response) => {
         setData(response.data.data);
       })
       .catch((error) => {
         console.error(error);
       });
-  }, []);
+  }, [page, sort]);
 
   const dataToMap = data.map((character) => <Card data={character} />);
   return (
     <div className={styles.container}>
       <h3>Characters</h3>
+      <div>
+        <Button
+          name="Name"
+          onClick={() => {
+            setSort("name");
+          }}
+        ></Button>
+        <Button
+          name="Planet"
+          onClick={() => {
+            setSort("planet");
+          }}
+        ></Button>
+        <Button
+          name="Gender"
+          onClick={() => {
+            setSort("gender");
+          }}
+        ></Button>
+      </div>
       <div>{dataToMap}</div>
+      <div>
+        <Button
+          name="Prev page"
+          onClick={() => {
+            page > 1 && setPage(page - 1);
+          }}
+        ></Button>
+        <Button
+          name="Next page"
+          onClick={() => {
+            page < 43 && setPage(page + 1);
+          }}
+        ></Button>
+      </div>
     </div>
   );
 };
